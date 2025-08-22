7d4b74da7c0f23f6b669b730abe5104599b81b31
VitinhoLeviski <20233024120@estudantes.ifpr.edu.br>
2025-08-21 23:28:49 -0300

    1ª versão

---
+++ Projeto_EscapeRoom/Assets/Internal Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_Manager : MonoBehaviour
+{
+    [SerializeField] private GameObject starterMenu;
+    [SerializeField] private GameObject setMenu;
+    [SerializeField] private GameObject starterMenuButtons;
+    [SerializeField] private GameObject setMenuButtons;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play() {
+        SceneManager.LoadScene("CutScene");
+    }
+
+    public void SetttingsMenuImage () {
+        starterMenuButtons.SetActive(false);
+        setMenuButtons.SetActive(true);
+    }
+
+
+}
