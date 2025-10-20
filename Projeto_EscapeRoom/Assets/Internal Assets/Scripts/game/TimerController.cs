1eaba6c8b77bc80772f58c275412e99a49a3b2af
VitinhoLeviski <20233024120@estudantes.ifpr.edu.br>
2025-10-20 10:27:56 -0300

    apresentado

--- Projeto_EscapeRoom/Assets/Internal Assets/Scripts/game/TimerController.cs
+++ Projeto_EscapeRoom/Assets/Internal Assets/Scripts/game/TimerController.cs
@@ -7,7 +7,7 @@ using UnityEngine.SceneManagement;
 public class TimerController : MonoBehaviour
 {
     [Header("Configurações do Timer")]
-    public float tempoInicial = 300f; // 5 minutos em segundos
+    public float tempoInicial = 302f; // 5 minutos em segundos
     private float tempoRestante;
 
     [Header("Referência UI")]
